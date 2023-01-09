16b71a25fbe936d3c66446bc6f89808fff854f55
maikeldiulio <maikeltecba@gmail.com>
2023-01-09 17:58:42 -0300

    segunda atividade typescript

---
+++ build/index.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const database_1 = require("./database");
+console.log("hello word");
+console.log("Usuário cadastrado!");
+console.table(database_1.users);
+console.log("produtos cadastrados");
+console.table(database_1.products);
+console.log("purchase");
+console.log(database_1.purchases);
+//# sourceMappingURL=index.js.map
